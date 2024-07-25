4bbff056032d7e4783d1360eb4d0539851e14266
kawtar <kawtarennabirha@gmail.com>
2024-07-25 12:17:37 +0100

    sinon

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,16 +1,11 @@
-(async () => {
-    const chai = await import('chai');
-    const sinon = await import('sinon');
-    const { expect } = chai;
-    const Utils = require('./utils.js');
-    const spy = sinon.spy(Utils, calculateNumber);
-    const sendPaymentRequestToApi = require('./3-payment.js');
-
-    describe('sendpaymentRequestToApi', function() {
-        it('spy', function() {
-            sendPaymentRequestToApi(100, 20);
-            expect(spy.calledOnce).to.be.true;
-            expect(spy.calledWith('SUM', 100, 20)).to.be.true;
-        });
-    });
-})();
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+describe('sendPaymentRequestToApi', () => {
+  it('should call calculateNumber', () => {
+    const spy = sinon.spy(Utils, 'calculateNumber');
+    sendPaymentRequestToApi(100, 20);
+    sinon.assert.calledWithMatch(spy, 'SUM', 100, 20);
+    spy.restore();
+  });
+});
