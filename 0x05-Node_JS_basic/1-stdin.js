af0bbe852c165750e8fb2d052877e876ee7cf0bc
kawtar <kawtarennabirha@gmail.com>
2024-07-26 11:03:20 +0100

    stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,15 @@
-var readline = require('readline');
-var r1 = readline.createInterface(process.stdin, process.stdout);
+const readline = require('readline');
+
+const r1 = readline.createInterface(process.stdin, process.stdout);
 
 console.log('Welcome to Holberton School, what is your name?');
 
 r1.question('', (name) => {
-    if (name !== null) {
-        console.log(`Your name is: ${name}`);
-    }
-   r1.close();
+  if (name !== null) {
+    console.log(`Your name is: ${name}`);
+  }
+  r1.close();
 });
 r1.on('close', () => {
-    console.log('This important software is now closing');
+  console.log('This important software is now closing');
 });
