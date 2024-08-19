386349b2ef8f50798359c516338391a2c359a784
kawtar <kawtarennabirha@gmail.com>
2024-08-19 17:28:21 +0100

    stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,7 @@
 console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.on('readable', () => {
-  let chunk;
-  while ((chunk = process.stdin.read()) !== null) {
-    process.stdout.write(`Your name is: ${chunk}`);
-  }
+process.stdin.on('data', (data) => {
+  console.log(`Your name is: ${data.toString()}`);
   process.exit();
 });
 
